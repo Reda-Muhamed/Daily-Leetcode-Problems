5fd9bc3f5f75ed6b1909f0af81f8beee125c9d5d
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2024-11-18 12:45:10 +0200

    Time: 5 ms (18.01%), Space: 50.5 MB (38.96%) - LeetHub

--- 1755-defuse-the-bomb/1755-defuse-the-bomb.js
+++ 1755-defuse-the-bomb/1755-defuse-the-bomb.js
@@ -11,12 +11,9 @@ var decrypt = function(code, k) {
     }else if(k>0){
         for(let i = 0;i<n;i++){
             let sum = 0;
-            let jj =k
-            for(let j = (i+1)%n ; ;j=(j+1)%n){
-                if(jj>0)
-                sum+=code[j]
-                else break
-                jj--;
+            for(let j = 1 ;j<=k ;j++){
+                sum+=code[(j+i+n)%n]
+            
             }
             res.push(sum)
         }
