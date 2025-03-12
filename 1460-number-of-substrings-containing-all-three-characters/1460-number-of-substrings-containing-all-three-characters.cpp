79be1c4550bfa30e9bdb9f7f52bb8eb5469aaf95
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2025-03-12 14:51:25 +0200

    Time: 14 ms (41.27%), Space: 11.2 MB (35.02%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.cpp
@@ -9,13 +9,11 @@ public:
         for (int right = 0; right < s.length(); ++right) {
             ++count[s[right] - 'a'];
 
-            // Check if the current window contains at least one 'a', 'b', and 'c'
             while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
                 --count[s[left] - 'a'];
                 ++left;
             }
 
-            // All substrings starting from any index before 'left' up to 'right' are valid
             ans += left;
         }
 
