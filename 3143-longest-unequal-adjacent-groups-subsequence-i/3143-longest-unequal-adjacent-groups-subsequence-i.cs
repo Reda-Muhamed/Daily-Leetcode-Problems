f37c6487c9027e3da924f939214a2ddb8d69f8c1
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2025-05-15 22:29:21 +0300

    Time: 0 ms (100%), Space: 57.2 MB (6.06%) - LeetHub

--- 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cs
+++ 3143-longest-unequal-adjacent-groups-subsequence-i/3143-longest-unequal-adjacent-groups-subsequence-i.cs
@@ -1,18 +1,20 @@
-using System;
-using System.Collections;
 public class Solution {
     public IList<string> GetLongestSubsequence(string[] words, int[] groups) {
-        int n = groups.Length;
-         Stack<int> s = new Stack<int>(); // store groups val
-         List<string>res = new List<string>();   
-        s.Push(groups[0]);
-        res.Add(words[0]);    
-        for(int i =1 ;i<n;i++){
-            if(groups[i]!=s.Peek()){
-                 s.Push(groups[i]);
-                  res.Add(words[i]);    
+         var result = new List<string>();
+        int n = words.Length;
+
+        if (n == 0) return result;
+
+        result.Add(words[0]);
+        int lastGroup = groups[0];
+
+        for (int i = 1; i < n; i++) {
+            if (groups[i] != lastGroup) {
+                result.Add(words[i]);
+                lastGroup = groups[i];
             }
         }
-        return res;
+
+        return result;
     }
 }
