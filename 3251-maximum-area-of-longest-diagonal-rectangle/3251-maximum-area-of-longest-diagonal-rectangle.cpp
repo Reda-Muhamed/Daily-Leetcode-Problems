d17d4ebbefcfcc95f9127f179689453d2a08257d
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2025-08-26 23:11:47 +0300

    Time: 0 ms (100%), Space: 29.3 MB (54.81%) - LeetHub

--- 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
+++ 3251-maximum-area-of-longest-diagonal-rectangle/3251-maximum-area-of-longest-diagonal-rectangle.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int areaOfMaxDiagonal(vector<vector<int>>& dimensions) {
-        int maxDiagSq = 0; // store max diagonal squared
+        int maxDiagSq = 0; 
         int maxArea = 0;
 
         for (auto &d : dimensions) {
             int w = d[0], h = d[1];
-            int diagSq = w * w + h * h; // compare squared diagonals
+            int diagSq = w * w + h * h;
             int area = w * h;
 
             if (diagSq > maxDiagSq || (diagSq == maxDiagSq && area > maxArea)) {
