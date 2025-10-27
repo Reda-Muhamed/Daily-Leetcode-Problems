d525acdaba25c2da3ea8dd0aa664cc1375211cb1
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2025-10-27 11:12:33 +0300

    Time: 183 ms (22.5%), Space: 52.9 MB (65%) - LeetHub

--- 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cs
+++ 2244-number-of-laser-beams-in-a-bank/2244-number-of-laser-beams-in-a-bank.cs
@@ -2,24 +2,18 @@ public class Solution
 {
     public int NumberOfBeams(string[] bank)
     {
-        List<int> ones = new List<int>();
+        int prev = 0, total = 0;
 
-        foreach (var b in bank)
+        foreach (var row in bank)
         {
-            int count = b.Count(ch => ch == '1');
-            if (count > 0)
-                ones.Add(count);
+            int curr = row.Count(ch => ch == '1');
+            if (curr > 0)
+            {
+                total += prev * curr;
+                prev = curr;
+            }
         }
 
-        if (ones.Count <= 1)
-            return 0;
-
-        int res = 0;
-        for (int i = 1; i < ones.Count; i++)
-        {
-            res += ones[i] * ones[i - 1];
-        }
-
-        return res;
+        return total;
     }
 }
