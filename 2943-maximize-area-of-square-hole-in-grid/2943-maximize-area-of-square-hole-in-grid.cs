856f4d939220fec178bee180972ba4af0e64c448
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-01-15 17:16:43 +0200

    Time: 3 ms (100%), Space: 45 MB (100%) - LeetHub

--- 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cs
+++ 2943-maximize-area-of-square-hole-in-grid/2943-maximize-area-of-square-hole-in-grid.cs
@@ -1,50 +1,26 @@
 using System;
 public class Solution {
     public int MaximizeSquareHoleArea(int n, int m, int[] hBars, int[] vBars) {
-        int mh=0 , mv=0;
-        Array.Sort(hBars);
-        Array.Sort(vBars);
+        int mh= maxConsecutive(hBars) , mv=maxConsecutive(vBars);
+        
+        return Math.Min(mv,mh)*Math.Min(mv,mh);
+    }
+    private int maxConsecutive( int[] hBars)
+    {
+        if (hBars.Length == 0) return 1;
 
-        int temp =1;
-        if(hBars.Length==1){
-            mh=2;
-        }
-                // 2,4,8,22,23,26
+        Array.Sort(hBars);
+         int temp =1,mh=1;
+        //2,3,5
         for(int i = 1 ;i<hBars.Length;i++){  
             if(hBars[i]-hBars[i-1]==1){
-                temp++;
-                if(i==hBars.Length - 1|| hBars[i+1]-hBars[i]!=1)
-                    temp++;
-                mh = Math.Max(temp,mh);
-                
+                temp++;               
             }else {
-                temp=1;
-                if(i==hBars.Length - 1|| hBars[i+1]-hBars[i]!=1 )
-                    temp++;
-                mh = Math.Max(temp,mh);
-                
+            temp=1;
             }
+            mh=Math.Max(mh,temp);
         }
-        if(vBars.Length==1){
-            mv=2;
-        }
-        temp=1;
-        //6,33,34,36
-        for(int i = 1 ;i<vBars.Length;i++){  
-            if(vBars[i]-vBars[i-1]==1){
-                temp++;
-                if(i==vBars.Length - 1 || vBars[i+1]-vBars[i]!=1 )
-                    temp++;
-                mv = Math.Max(temp,mv);
-                
-            }else {
-                temp=1;
-                if(i==vBars.Length - 1|| vBars[i+1]-vBars[i]!=1)
-                    temp++;
-                mv = Math.Max(temp,mv);
-            }
-        }
-        return Math.Min(mv,mh)*Math.Min(mv,mh);
-        
-    }
+        return ++mh;
+
+    }       
 }
