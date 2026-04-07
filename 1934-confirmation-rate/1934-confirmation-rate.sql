b702c4120c124215145054f1e24083a9b78c61a0
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-07 22:10:03 +0200

    Time: 640 ms (52.07%), Space: 0B (100%) - LeetHub

--- 1934-confirmation-rate/1934-confirmation-rate.sql
+++ 1934-confirmation-rate/1934-confirmation-rate.sql
@@ -1,5 +1,5 @@
 select s.user_id,  
-COALESCE(round(count(case when action = 'confirmed' then action end) / count(action),2),0) as confirmation_rate
+round(ifnull(avg(if(action = 'confirmed' , 1 , 0)),0),2) as confirmation_rate
 from Signups s
 left join Confirmations c on s.user_id = c.user_id
 group by s.user_id
