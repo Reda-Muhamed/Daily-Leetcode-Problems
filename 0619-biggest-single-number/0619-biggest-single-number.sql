192ce28224bdca9bdd4fefa81b8058adc0a92a8d
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-10 20:27:19 +0200

    Time: 398 ms (72.43%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,7 +1,14 @@
 # Write your MySQL query statement below
-with temp as (
-    select num from MyNumbers
-    group by num
-    having(count(num) = 1)
-)
-select max(num) as num from temp
+-- with temp as (
+--     select num from MyNumbers
+--     group by num
+--     having(count(num) = 1)
+-- )
+-- select max(num) as num from temp
+-- 
+-- Efficient solution
+--
+select case when count(num) = 1 then num else null end as num
+from MyNumbers 
+group by num
+order by num desc limit 1
