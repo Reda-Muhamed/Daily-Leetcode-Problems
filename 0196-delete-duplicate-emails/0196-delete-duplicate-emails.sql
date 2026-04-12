da8f003e8d39c385f93f11a485408859d6dc0ac9
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-12 17:50:43 +0200

    Time: 557 ms (89.41%), Space: 0B (100%) - LeetHub

--- 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
+++ 0196-delete-duplicate-emails/0196-delete-duplicate-emails.sql
@@ -1,3 +1,11 @@
 
-delete p1 from Person p1
- join Person p2 on p2.email = p1.email and p1.id > p2.id
+-- delete p1 from Person p1
+-- join Person p2 on p2.email = p1.email and p1.id > p2.id
+
+-- Another solution
+
+delete from Person where id not in (
+    select minid from (
+        select min(id) as minid from Person group by email
+    )as t
+)
