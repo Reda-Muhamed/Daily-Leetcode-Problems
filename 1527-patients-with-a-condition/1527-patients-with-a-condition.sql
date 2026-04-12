dada0dc0de9a486d059c975ae14795f45de5de74
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-12 17:37:18 +0200

    Time: 435 ms (17.65%), Space: 0B (100%) - LeetHub

--- 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
+++ 1527-patients-with-a-condition/1527-patients-with-a-condition.sql
@@ -1,8 +1,6 @@
 # Write your MySQL query statement below
--- select patient_id, patient_name, conditions 
--- from Patients
--- where  REGEXP_LIKE(conditions, '^DIAB1','c') or REGEXP_LIKE(conditions, '\\sDIAB1','c')
-SELECT patient_id, patient_name, conditions
-FROM Patients
-WHERE conditions LIKE 'DIAB1%'      -- DIAB1 at start
-   OR conditions LIKE '% DIAB1%'    -- DIAB1 in middle or end
+
+select patient_id, patient_name, conditions
+from Patients
+where conditions like 'DIAB1%'      
+   or conditions like '% DIAB1%'    
