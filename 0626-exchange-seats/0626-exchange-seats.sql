41ffb51234d70e2559c11764e852917a4ca30744
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-12 13:47:07 +0200

    Time: 341 ms (47.02%), Space: 0B (100%) - LeetHub

--- 0626-exchange-seats/0626-exchange-seats.sql
+++ 0626-exchange-seats/0626-exchange-seats.sql
@@ -1,4 +1,11 @@
 # Write your MySQL query statement below
-select id ,
- if(id % 2 = 1, if((lead(student) over (order by id)) is not null , lead(student) over (order by id) , student),
-lag(student) over (order by id) ) as student from Seat
+-- select id ,
+--  if(id % 2 = 1, if((lead(student) over (order by id)) is not null , lead(student) over (order by id) , student),
+-- lag(student) over (order by id) ) as student from Seat
+
+-- Efficient solution 
+
+select if(id % 2 = 1 , if(id = (select id from Seat order by id desc limit 1) , id , id + 1) , id - 1 ) as id , student
+from Seat 
+group by id
+order by id
