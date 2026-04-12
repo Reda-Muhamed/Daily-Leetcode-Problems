cdb99242b270caa78edb7551094a2995272fcec5
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-12 17:34:35 +0200

    Time: 576 ms (95.92%), Space: 0B (100%) - LeetHub

--- 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
+++ 1667-fix-names-in-a-table/1667-fix-names-in-a-table.sql
@@ -1,14 +1,17 @@
 # Write your MySQL query statement below
-select user_id ,  IF(
-    name LIKE '% %',
-  CONCAT(
-  upper(left(SUBSTRING_INDEX(name, ' ', 1),1)),
-  lower(SUBSTRING(SUBSTRING_INDEX(name,' ',1),2)),
-  ' ',
-  lower(SUBSTRING_INDEX(name, ' ', -1))
-),  CONCAT(
-  upper(left(SUBSTRING_INDEX(name, ' ', 1),1)),
-  lower(SUBSTRING(SUBSTRING_INDEX(name,' ',1),2)))
- ) as name
-from Users 
+-- select user_id ,
+-- if(
+--     name like '% %',
+--     concat(upper(left(substring_index(name, ' ', 1),1)),lower(substring(substring_index(name,' ',1),2)),' ',lower(substring_index(name, ' ', 2)))
+--   ,  
+--   concat(
+--     upper(left(substring_index(name, ' ', 1),1)),
+--     lower(substring(substring_index(name,' ',1),2)))
+--  ) as name
+-- from Users
+-- order by user_id
+
+-- Efficient solution
+select user_id ,concat(upper(left(name,1)),lower(substring(name,2))) as name
+from Users
 order by user_id
