4d870a9ace3761c7315ffc11f7dfcacc7150cf86
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-04-12 17:13:40 +0200

    Time: 1190 ms (25.86%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,16 +1,28 @@
 # Write your MySQL query statement below
 
-with ranked_employees as (
-    select 
-        departmentid, 
-        name,
-        salary,
-        dense_rank() over (partition by departmentid order by salary desc) as row_num
-    from Employee 
-),
-top_ranked  as (
-    select * from ranked_employees where row_num <=3 
+-- with ranked_employees as (
+--     select 
+--         departmentid, 
+--         name,
+--         salary,
+--         dense_rank() over (partition by departmentid order by salary desc) as row_num
+--     from Employee 
+-- ),
+-- top_ranked  as (
+--     select * from ranked_employees where row_num <=3 
+-- )
+-- select d.name as Department, t.name as Employee , t.salary
+--  from top_ranked t 
+--  join Department d on t.departmentid = d.id  
+
+-- Efficient Solution 
+with cte as (
+    select *, dense_rank() over(partition by departmentId order by salary desc) as rnk
+    from Employee
 )
-select d.name as Department, t.name as Employee , t.salary
- from top_ranked t 
- join Department d on t.departmentid = d.id  
+
+select d.name as Department, c.name as Employee, c.salary as Salary
+from Department d
+join cte c
+on d.id = c.departmentId
+where rnk in (1,2,3)
