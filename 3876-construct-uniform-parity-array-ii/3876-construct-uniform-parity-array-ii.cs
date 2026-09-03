b1312ecb3104912c1a88f1ddd1a5e2ec4ea1aa10
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-09-03 12:48:16 +0300

    Time: 9 ms (40%), Space: 80.2 MB (20%) - LeetHub

--- 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cs
+++ 3876-construct-uniform-parity-array-ii/3876-construct-uniform-parity-array-ii.cs
@@ -1,15 +1,13 @@
 public class Solution {
     public bool UniformArray(int[] nums1) {
        int temp = nums1.Min();
-       if (temp % 2 == 0) {
+       if ((temp & 1) == 0) {
             for (int i = 0 ;i<nums1.Length; i++) {
                 if ((nums1[i] & 1) == 1) 
                     return false;
             }
             return true;
        }
-       else {
-            return true;
-        }
+        return true;
     }    
 }
