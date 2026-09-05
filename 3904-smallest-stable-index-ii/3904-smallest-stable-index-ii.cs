98ee2e1d43aff8ea3f8c7160cb00c7760baa0e27
Reda <166030682+Reda-Muhamed@users.noreply.github.com>
2026-09-05 15:47:54 +0300

    Time: 10 ms (46.15%), Space: 77.9 MB (46.15%) - LeetHub

--- 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cs
+++ 3904-smallest-stable-index-ii/3904-smallest-stable-index-ii.cs
@@ -1,37 +1,27 @@
-public class Solution {
-    public int FirstStableIndex(int[] nums, int k) {
+public class Solution
+{
+    public int FirstStableIndex(int[] nums, int k)
+    {
         int len = nums.Length;
-        int[] prefMax = new int[len];
-        prefMax[0] = nums[0];
-        
-        for (int i = 1; i < len ; i++) {
-            if (nums[i] > prefMax[i-1]) {
-                prefMax[i] = nums[i];
-            } else {
-                prefMax[i] = prefMax[i - 1];
-            }
-        }
 
         int[] suffixMin = new int[len];
-
         suffixMin[len - 1] = nums[len - 1];
 
         for (int i = len - 2; i >= 0; i--)
         {
-            if (nums[i] < suffixMin[i + 1])
-            {
-                suffixMin[i] = nums[i];
-            }
-            else
-            {
-                suffixMin[i] = suffixMin[i + 1];
-            }
+            suffixMin[i] = Math.Min(nums[i], suffixMin[i + 1]);
         }
-        for (int i = 0; i < len; i++) {
-            if(prefMax[i] - suffixMin[i] <= k)
+
+        int prefMax = nums[0];
+
+        for (int i = 0; i < len; i++)
+        {
+            prefMax = Math.Max(prefMax, nums[i]);
+
+            if (prefMax - suffixMin[i] <= k)
                 return i;
         }
+
         return -1;
     }
-    
 }
